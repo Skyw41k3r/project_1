62b5f34812a57ae841e7902fa0208d4d364bcef6
Skyw41k3r <mwalk369@gmail.com>
2022-04-14 21:39:25 -0700

    script.js

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -18,6 +18,23 @@ function searchSubmission(event) {
 //This code is for the fetch api//const encodedParams = new URLSearchParams();//
 encodedParams.append("accessToken", "<REQUIRED>");
 encodedParams.append("userId", "<REQUIRED>");
+const data = "accessToken=%3CREQUIRED%3E&userId=%3CREQUIRED%3E";
+
+const xhr = new XMLHttpRequest();
+xhr.withCredentials = true;
+
+xhr.addEventListener("readystatechange", function() {
+    if (this.readyState === this.DONE) {
+        console.log(this.responseText);
+    }
+});
+
+xhr.open("POST", "https://anilistmikilior1v1.p.rapidapi.com/getAnimeList");
+xhr.setRequestHeader("content-type", "application/x-www-form-urlencoded");
+xhr.setRequestHeader("X-RapidAPI-Host", "Anilistmikilior1V1.p.rapidapi.com");
+xhr.setRequestHeader("X-RapidAPI-Key", "cfbae33ec0mshf109c03e93a6396p1b5151jsnff154fb8b1f8");
+
+xhr.send(data);
 
 const options = {
     method: 'POST',
@@ -33,3 +50,44 @@ fetch('https://anilistmikilior1v1.p.rapidapi.com/getAnimeList', options)
     .then(response => response.json())
     .then(response => console.log(response))
     .catch(err => console.error(err));
+const settings = {
+    "async": true,
+    "crossDomain": true,
+    "url": "https://anilistmikilior1v1.p.rapidapi.com/getAnimeList",
+    "method": "POST",
+    "headers": {
+        "content-type": "application/x-www-form-urlencoded",
+        "X-RapidAPI-Host": "Anilistmikilior1V1.p.rapidapi.com",
+        "X-RapidAPI-Key": "cfbae33ec0mshf109c03e93a6396p1b5151jsnff154fb8b1f8"
+    },
+    "data": {
+        "accessToken": "<REQUIRED>",
+        "userId": "<REQUIRED>"
+    }
+};
+
+$.ajax(settings).done(function(response) {
+    console.log(response);
+});
+import axios from "axios";
+
+const encodedParams = new URLSearchParams();
+encodedParams.append("accessToken", "<REQUIRED>");
+encodedParams.append("userId", "<REQUIRED>");
+
+const options = {
+    method: 'POST',
+    url: 'https://anilistmikilior1v1.p.rapidapi.com/getAnimeList',
+    headers: {
+        'content-type': 'application/x-www-form-urlencoded',
+        'X-RapidAPI-Host': 'Anilistmikilior1V1.p.rapidapi.com',
+        'X-RapidAPI-Key': 'cfbae33ec0mshf109c03e93a6396p1b5151jsnff154fb8b1f8'
+    },
+    data: encodedParams
+};
+
+axios.request(options).then(function(response) {
+    console.log(response.data);
+}).catch(function(error) {
+    console.error(error);
+});
