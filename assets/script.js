c84ddf99bf9bfd880af268cda99d8ec7ee8a1ec3
rensyphon <rensyphon@gmail.com>
2022-04-16 11:38:39 -0700

    youtube api fetch

---
+++ assets/script.js
@@ -0,0 +1,29 @@
+var searchEl = document.querySelector("#search");
+var buttonEl = document.querySelector("button");
+var apiKey = "AIzaSyAuXIW5JqAMLqXas2a7CADZA-elSAnHb5w";
+var vidIdTag;
+var vidUrl;
+
+function onSearch(searchValue) {
+    var requestURL = "https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=1&q=" + searchValue + "%20trailer&key=" + apiKey;
+    fetch(requestURL) 
+    .then(function(response){
+        console.log(response);
+        return response.json();
+    }).then(function(data){
+        vidIdTag = data.items[0].id.videoId;
+        vidUrl = "https://www.youtube.com/watch?v=" + vidIdTag;
+        console.log(vidIdTag);
+        console.log (vidUrl);
+        console.log(data);
+    }).catch(function(error){
+        console.log(error);
+    })
+}
+
+buttonEl.addEventListener("click", function(event){
+    event.preventDefault()
+    var value = searchEl.value;
+    console.log(value);
+    onSearch(value);
+})
