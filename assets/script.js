0d5b67a87c1c58c6888ccf97b00f8ec107f8d7b8
Skyw41k3r <mwalk369@gmail.com>
2022-04-16 13:08:40 -0700

    java stuff

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,19 @@
-//mike trying to get this to work
+const settings = {
+    "async": true,
+    "crossDomain": true,
+    "url": "https://anilistmikilior1v1.p.rapidapi.com/getAnimeList",
+    "method": "POST",
+    "headers": {
+        "content-type": "application/x-www-form-urlencoded",
+        "X-RapidAPI-Host": "Anilistmikilior1V1.p.rapidapi.com",
+        "X-RapidAPI-Key": "cfbae33ec0mshf109c03e93a6396p1b5151jsnff154fb8b1f8"
+    },
+    "data": {
+        "accessToken": "<REQUIRED>",
+        "userId": "<REQUIRED>"
+    }
+};
+
+$.ajax(settings).done(function(response) {
+    console.log(response);
+});
