db1ef85a62cebacdb2e6a692fb7e58955f57f51c
Leandro <machadoleandro98@gmail.com>
2022-04-21 21:00:21 -0700

    Go back up

--- assets/script.js
+++ assets/script.js
@@ -97,4 +97,16 @@ fetch(requestURL)
 })
 }
 
+var scrollToTopBtn = document.getElementById("scrollToTopBtn");
+var rootElement = document.documentElement;
+
+function scrollToTop() {
+  // Scroll to top logic
+  rootElement.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+}
+scrollToTopBtn.addEventListener("click", scrollToTop);
+
 buttonEl.addEventListener("click", searchSubmission)
